a6bb3e637344b918909f53bc769a0e22bb08397a
Brandon <nardnob@users.noreply.github.com>
2018-12-08 19:47:13 -0600

    Initial commit

---
+++ Pong VB/Pong/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pnlPlayer = New System.Windows.Forms.Panel()
+        Me.pnlMob = New System.Windows.Forms.Panel()
+        Me.pnlBall = New System.Windows.Forms.Panel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlBack = New System.Windows.Forms.Panel()
+        Me.lblScoreMob = New System.Windows.Forms.Label()
+        Me.lblScorePlayer = New System.Windows.Forms.Label()
+        Me.pnlBack.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlPlayer
+        '
+        Me.pnlPlayer.BackColor = System.Drawing.Color.White
+        Me.pnlPlayer.Location = New System.Drawing.Point(12, 12)
+        Me.pnlPlayer.Name = "pnlPlayer"
+        Me.pnlPlayer.Size = New System.Drawing.Size(10, 93)
+        Me.pnlPlayer.TabIndex = 0
+        '
+        'pnlMob
+        '
+        Me.pnlMob.BackColor = System.Drawing.Color.White
+        Me.pnlMob.Location = New System.Drawing.Point(1035, 567)
+        Me.pnlMob.Name = "pnlMob"
+        Me.pnlMob.Size = New System.Drawing.Size(10, 93)
+        Me.pnlMob.TabIndex = 1
+        '
+        'pnlBall
+        '
+        Me.pnlBall.BackColor = System.Drawing.Color.White
+        Me.pnlBall.Location = New System.Drawing.Point(536, 336)
+        Me.pnlBall.Name = "pnlBall"
+        Me.pnlBall.Size = New System.Drawing.Size(10, 10)
+        Me.pnlBall.TabIndex = 1
+        '
+        'Timer1
+        '
+        '
+        'pnlBack
+        '
+        Me.pnlBack.BackColor = System.Drawing.Color.Transparent
+        Me.pnlBack.Controls.Add(Me.lblScoreMob)
+        Me.pnlBack.Controls.Add(Me.lblScorePlayer)
+        Me.pnlBack.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlBack.Location = New System.Drawing.Point(0, 0)
+        Me.pnlBack.Name = "pnlBack"
+        Me.pnlBack.Size = New System.Drawing.Size(1057, 673)
+        Me.pnlBack.TabIndex = 2
+        '
+        'lblScoreMob
+        '
+        Me.lblScoreMob.AutoSize = True
+        Me.lblScoreMob.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScoreMob.Location = New System.Drawing.Point(630, 27)
+        Me.lblScoreMob.Name = "lblScoreMob"
+        Me.lblScoreMob.Size = New System.Drawing.Size(30, 31)
+        Me.lblScoreMob.TabIndex = 1
+        Me.lblScoreMob.Text = "0"
+        '
+        'lblScorePlayer
+        '
+        Me.lblScorePlayer.AutoSize = True
+        Me.lblScorePlayer.Font = New System.Drawing.Font("Courier New", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScorePlayer.Location = New System.Drawing.Point(430, 27)
+        Me.lblScorePlayer.Name = "lblScorePlayer"
+        Me.lblScorePlayer.Size = New System.Drawing.Size(30, 31)
+        Me.lblScorePlayer.TabIndex = 0
+        Me.lblScorePlayer.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1057, 672)
+        Me.Controls.Add(Me.pnlBall)
+        Me.Controls.Add(Me.pnlMob)
+        Me.Controls.Add(Me.pnlPlayer)
+        Me.Controls.Add(Me.pnlBack)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "VB Pong"
+        Me.pnlBack.ResumeLayout(False)
+        Me.pnlBack.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlPlayer As System.Windows.Forms.Panel
+
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents pnlMob As System.Windows.Forms.Panel
+    Friend WithEvents pnlBall As System.Windows.Forms.Panel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents pnlBack As System.Windows.Forms.Panel
+    Friend WithEvents lblScoreMob As System.Windows.Forms.Label
+    Friend WithEvents lblScorePlayer As System.Windows.Forms.Label
+
+End Class
---
+++ Pong VB/Pong/Form1.vb
@@ -0,0 +1,190 @@
+﻿Public Class Form1
+
+    Private screenY As Integer
+    Private screenH As Integer
+
+    Private ballX As Integer
+    Private ballY As Integer
+
+    Private mobVelY As Double = 6
+    Private mobUp As Integer = 1
+
+    Private maxVelInit As Integer = 10
+    Private maxVel As Integer = maxVelInit
+    Private maxMaxVel As Integer = 30
+    Private velX As Integer
+    Private velY As Integer
+    Private maxVelIncrement As Integer = 2 'The amount to increment the ball's velocity per hit
+
+    'Heights, decreases for the winner proportional to their winning ratio
+    Private playerHeightInit As Integer = 96
+    Private playerHeight As Integer = playerHeightInit
+    Private mobHeightInit As Integer = 96
+    Private mobHeight As Integer = mobHeightInit
+
+    Private Const winTop = 30
+    Private Const winBottom = 5
+
+    Private scorePlayer As Integer = 0
+    Private scoreMob As Integer = 0
+
+    Private restart As Boolean = True
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        screenY = pnlBack.Location.Y + pnlBack.Location.Y
+        screenH = pnlBack.Size.Height
+
+        ' Add any initialization after the InitializeComponent() call.
+        Timer1.Interval = 0.7
+        Timer1.Start()
+
+        'Initialize the random number generator
+        Randomize()
+
+        'Hide the cursor
+        Cursor.Hide()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+
+        'Restart the ball's position if required
+        If restart Then
+            pnlBall.Location = New Point(536, 336)
+
+            'Reset the maxVel
+            maxVel = maxVelInit
+
+            'Add a bit of randomness to the ball's direction. Still includes the current maximum velocity
+            velX = (maxVel * 0.5 - maxVel * 0.2 + 1) * Rnd() + maxVel * 0.2
+            If velX <> 0 Then
+                velY = Math.Sqrt(Math.Pow(maxVel, 2) / Math.Pow(velX, 2))
+            Else
+                velY = maxVel
+            End If
+            restart = False
+        End If
+
+        'Update the location & height of the screen
+        screenY = Me.Location.Y + pnlBack.Location.Y
+        screenH = pnlBack.Size.Height
+
+        'Update the players' location based on the mouse position w/ respect to screenY
+        pnlPlayer.Location = New Point(pnlPlayer.Location.X, MousePosition.Y - screenY - pnlPlayer.Size.Height / 2 - winTop)
+        pnlBack.PointToClient(pnlPlayer.Location)
+
+        'Bound the player inside the screen
+        If pnlPlayer.Location.Y < 0 Then
+            pnlPlayer.Location = New Point(pnlPlayer.Location.X, 0)
+        ElseIf pnlPlayer.Location.Y + pnlPlayer.Size.Height > screenH Then
+            pnlPlayer.Location = New Point(pnlPlayer.Location.X, screenH - pnlPlayer.Size.Height)
+        End If
+
+        'Ball velocity to movement
+        pnlBall.Location = New Point(pnlBall.Location.X + velX, pnlBall.Location.Y + velY)
+        ballX = pnlBall.Location.X + pnlBall.Size.Width / 2
+        ballY = pnlBall.Location.Y + pnlBall.Size.Height / 2
+
+        'Bound the ball vertically
+        If ballY + velY < 0 Or ballY + velY > screenH Then
+            velY = -velY
+        End If
+
+        'Bound the ball horizontally between the player and mob
+        If (ballX + velX >= pnlPlayer.Location.X And ballX + velX <= pnlPlayer.Location.X + pnlPlayer.Size.Width) And (ballY + velY >= pnlPlayer.Location.Y And ballY + velY <= pnlPlayer.Location.Y + pnlPlayer.Size.Height) Then
+
+            'Reverse the velX
+            velX = -velX
+
+            'Create vel ratios to the current maxVel to then update vel with the new max vel
+            Dim ratioX As Double = velX / maxVel
+            Dim ratioY As Double = velY / maxVel
+
+
+            'Increase the max vel to increase difficulty per hit
+            maxVel += maxVelIncrement
+
+            If maxVel > maxMaxVel Then
+                maxVel = maxMaxVel
+            End If
+
+            'Update the current vel using the ratios
+            velX = ratioX * maxVel
+            velY = ratioY * maxVel
+
+        ElseIf (ballX + velX >= pnlMob.Location.X And ballX + velX <= pnlMob.Location.X + pnlMob.Size.Width) And (ballY + velY >= pnlMob.Location.Y And ballY + velY <= pnlMob.Location.Y + pnlMob.Size.Height) Then
+
+            'Reverse the velX
+            velX = -velX
+
+            'Create vel ratios to the current maxVel to then update vel with the new max vel
+            Dim ratioX As Double = velX / maxVel
+            Dim ratioY As Double = velY / maxVel
+
+
+            'Increase the max vel to increase difficulty per hit
+            maxVel += maxVelIncrement
+
+            If maxVel > maxMaxVel Then
+                maxVel = maxMaxVel
+            End If
+
+            'Update the current vel using the ratios
+            velX = ratioX * maxVel
+            velY = ratioY * maxVel
+
+        End If
+
+        'Mob AI intention to follow the ball
+        If ballY > pnlMob.Location.Y + pnlMob.Size.Height / 2 And velX > 0 Then
+            mobUp = 1
+        ElseIf velX > 0 Then
+            mobUp = -1
+        Else
+            mobUp = 0
+        End If
+
+        'Mob intention to vel
+        pnlMob.Location = New Point(pnlMob.Location.X, pnlMob.Location.Y + mobVelY * mobUp)
+
+        'Bound the mob to the screen
+        If pnlMob.Location.Y < 0 Then
+            pnlMob.Location = New Point(pnlMob.Location.X, 0)
+        ElseIf pnlMob.Location.Y + pnlMob.Size.Height > screenH Then
+            pnlMob.Location = New Point(pnlMob.Location.X, screenH - pnlMob.Size.Height)
+        End If
+
+        'Check for a score.. Mob then Player
+        If ballX < pnlPlayer.Location.X Then
+            scoreMob += 1
+            lblScoreMob.Text = scoreMob.ToString()
+            restart = True
+        ElseIf ballX > pnlMob.Location.X + pnlMob.Size.Width Then
+            scorePlayer += 1
+            lblScorePlayer.Text = scorePlayer.ToString()
+            restart = True
+        End If
+
+        'Bound the mouse cursor inside the window
+        If Cursor.Position.X < Me.Location.X + 10 Then
+            Cursor.Position = New Point(Me.Location.X + 10, Cursor.Position.Y)
+        ElseIf Cursor.Position.X > Me.Size.Width + Me.Location.X - 10 Then
+            Cursor.Position = New Point(Me.Size.Width + Me.Location.X - 10, Cursor.Position.Y)
+        End If
+        If Cursor.Position.Y < Me.Location.Y + 20 Then
+            Cursor.Position = New Point(Cursor.Position.X, Me.Location.Y + 20)
+        ElseIf Cursor.Position.Y > Me.Size.Height + Me.Location.Y - 10 Then
+            Cursor.Position = New Point(Cursor.Position.X, Me.Size.Height + Me.Location.Y - 10)
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = ChrW(Keys.Escape) Then
+            Me.Close()
+        End If
+    End Sub
+End Class
